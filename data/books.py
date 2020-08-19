d663b60dd0305f4f1d28ebd181e8aad184e3b896
BEE1214 <advorsky.gos@gmail.com>
2020-08-19 15:38:46 +0200

    Revert "delete of unneeded folders"
    This reverts commit 0b04735506203bc96d8e8b299864b56f5454f77c.

---
+++ data/books.py
@@ -0,0 +1,30 @@
+##=================================##
+##=-Project:           BKControl -=##
+##=-Author:         Adam Dvorsky -=##
+##=-Date:             2020-08-17 -=##
+##=================================##
+
+class books:
+    # iBooks = {
+    #     "Studijní předpoklady a základy logiky 1. díl" : "studijni-predpoklady-a-zaklady-logiky-1.-dil",
+    #     "Studijní předpoklady a základy logiky 2. díl" : "studijni-predpoklady-a-zaklady-logiky-2.-dil",
+
+    # }
+
+    iBooks = [
+        'Studijní předpoklady a základy logiky 1. díl',
+        'Studijní předpoklady a základy logiky 2. díl'
+    ]
+
+    iRefs = [
+        'studijni-predpoklady-a-zaklady-logiky-1-dil',
+        'testy-obecnych-studijnich-predpokladu-a-zaklady-logiky-2-dil'
+    ]
+
+
+class bookstores:
+    iBookstores = {
+        'Dobrovsky' : 'https://www.knihydobrovsky.cz/',
+        'Kosmas' : 'https://www.kosmas.cz/',
+
+    }
---
+++ data/bookstores.py
@@ -0,0 +1,12 @@
+##=================================##
+##=-Project:           BKControl -=##
+##=-Author:         Adam Dvorsky -=##
+##=-Date:             2020-08-17 -=##
+##=================================##
+
+class bookstores:
+    iBookstores = {
+        'Dobrovsky' : 'https://www.knihydobrovsky.cz/',
+        'Kosmas' : 'https://www.kosmas.cz/',
+
+    }
